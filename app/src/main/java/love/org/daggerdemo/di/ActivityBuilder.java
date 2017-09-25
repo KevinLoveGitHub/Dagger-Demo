176c9efb59beb82fb6b2a5f66c3d50140570b474
Kevin <616651408@qq.com>
2017-09-25 18:42:13 +0800

    Update structure

--- app/src/main/java/love/org/daggerdemo/di/ActivityBuilder.java
+++ app/src/main/java/love/org/daggerdemo/di/ActivityBuilder.java
@@ -8,8 +8,6 @@ import love.org.daggerdemo.ui.MainActivityModule;
 @Module
 public abstract class ActivityBuilder {
 
-    // TODO #3.2 - Map the Activity to the Builder
     @ContributesAndroidInjector(modules = MainActivityModule.class)
     abstract MainActivity bindMainActivity();
-
 }
--- app/src/main/java/love/org/daggerdemo/ui/MainActivityModule.java
+++ app/src/main/java/love/org/daggerdemo/ui/MainActivityModule.java
@@ -6,16 +6,11 @@ import dagger.Provides;
 @Module
 public class MainActivityModule {
 
-    // TODO #2.2 - Provides dependency to MainView.
     @Provides
     MainView provideMainView(MainActivity mainActivity) {
         return mainActivity;
     }
 
-    // TODO #2.2 - Provides dependency to MainPresenter
-    // params:
-    //      MainView - dependency will come from #2.2 - MainActivityModule#provideMainView(MainActivity):MainView
-    //      ApiService - dependency will from from #1.2 AppModule#provideApiService():ApiService
 
     @Provides
     MainPresenter provideMainPresenter(MainView mainView) {
